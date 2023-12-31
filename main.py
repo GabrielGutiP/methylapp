5105e0700364194dd0107f7e3f7116a30e29f3db
GabrielGutiP <146023114+GabrielGutiP@users.noreply.github.com>
2023-12-31 19:24:47 +0100

    Initial upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter
+
+window = tkinter.Tk()
+window.geometry("400x300")
+
+label = tkinter.Label(window, text="MethylApp")
+
+
+window.mainloop()                                   # "Log" de sucesos en la ventana
